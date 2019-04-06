19d01301668cc7f1fae03b7539138c63744ca150
ohatakky <ohatakky@gmail.com>
2019-04-06 17:14:11 +0900

    routingはmux

--- f/7.go
+++ f/7.go
@@ -11,6 +11,10 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 // ブラウザへの書き込みに使えるhttp.ResponseWriterも、io.Writerインタフェースの同じWrite()メソッドで書き出すことができる。
 func HttpWrite() {
+	// https://qiita.com/awakia/items/ef8978fc33ffb1d27460
+	// HandleFuncすると、DefaultServerMuxと言うものにマッピングが登録される
+	// muxはマルチプレクサのこと
+	// 複数のリクエストがあって、一致した1つを出力とするルーティングは、マルチプレクサの動作に似ている？？？
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
